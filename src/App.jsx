5ff42327ef0d580c1aa0b5cb805c31d551d69114
WIN-ADK7AOT0SOJ\ICT02_20 <sally940828@naver.com>
2024-10-11 11:51:16 +0900

    로또볼 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column vh-100 justify-content-between">
+      <Header />
+      <Outlet />
+      {/*⭐ URL에 따라 변경되는 부분 ⭐ */}
       <Footer></Footer>
-    </>
+    </div>
   );
 }
 
---
+++ src/router/Router.jsx
@@ -0,0 +1,24 @@
+import { children } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Footer from "./../components/layout/Footer";
+import LottoBall from "../components/lotto/LottoBall";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    loader: () => "로또",
+    children: [
+      {
+        path: "/",
+        loader: () => "로또",
+        element: <LottoBall />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+export { router, routes };
